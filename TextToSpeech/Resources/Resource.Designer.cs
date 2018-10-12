0fd647c9f8843cc467b486bfa5d4193a725236d3
Kuldeep Thakre <kuldeep-kd@live.com>
2018-10-12 23:59:11 +0530

    Initial Background Concept

--- TextToSpeech/Resources/Resource.Designer.cs
+++ TextToSpeech/Resources/Resource.Designer.cs
@@ -41,35 +41,51 @@ namespace TextToSpeech
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int Image = 2130837504;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060008
-			public const int btnPlay = 2131099656;
+			// aapt resource value: 0x7f070008
+			public const int btnPlay = 2131165192;
 			
-			// aapt resource value: 0x7f060007
-			public const int langspinner = 2131099655;
+			// aapt resource value: 0x7f070007
+			public const int langspinner = 2131165191;
 			
-			// aapt resource value: 0x7f060004
-			public const int seekerPitch = 2131099652;
+			// aapt resource value: 0x7f070004
+			public const int seekerPitch = 2131165188;
 			
-			// aapt resource value: 0x7f060002
-			public const int seekerSpeed = 2131099650;
+			// aapt resource value: 0x7f070002
+			public const int seekerSpeed = 2131165186;
 			
-			// aapt resource value: 0x7f060006
-			public const int seekerVolume = 2131099654;
+			// aapt resource value: 0x7f070006
+			public const int seekerVolume = 2131165190;
 			
-			// aapt resource value: 0x7f060000
-			public const int textBox = 2131099648;
+			// aapt resource value: 0x7f070000
+			public const int textBox = 2131165184;
 			
-			// aapt resource value: 0x7f060003
-			public const int textPitch = 2131099651;
+			// aapt resource value: 0x7f070003
+			public const int textPitch = 2131165187;
 			
-			// aapt resource value: 0x7f060001
-			public const int textSpeed = 2131099649;
+			// aapt resource value: 0x7f070001
+			public const int textSpeed = 2131165185;
 			
-			// aapt resource value: 0x7f060005
-			public const int textVolume = 2131099653;
+			// aapt resource value: 0x7f070005
+			public const int textVolume = 2131165189;
 			
 			static Id()
 			{
@@ -84,8 +100,8 @@ namespace TextToSpeech
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Main = 2130968576;
 			
 			static Layout()
 			{
@@ -100,8 +116,8 @@ namespace TextToSpeech
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -116,11 +132,11 @@ namespace TextToSpeech
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
@@ -135,8 +151,8 @@ namespace TextToSpeech
 		public partial class Style
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int Theme_Custom = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int Theme_Custom = 2131099648;
 			
 			static Style()
 			{
